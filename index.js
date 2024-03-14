2d626b30e5f1cbbb2ac06a153e12b2bfa8fff6b6
tpeck428 <toripeck73@gmail.com>
2024-03-14 18:19:12 -0400

    added h1 element

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//Part One: Getting Started
+
+//Select and cache the <main> element in a variable named mainEl.
 let mainEl = document.querySelector('main');
 console.log(mainEl);
 
-mainEl.style.backgroundColor = '#4a4e4d';
+//Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+mainEl.style.backgroundColor = "var(--main-bg)";
 
+//Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do 
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+//Add a class of flex-ctr to mainEl.
